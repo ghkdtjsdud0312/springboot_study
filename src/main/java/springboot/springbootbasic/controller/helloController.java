643a290f756900e807b9e738a879d1172fbfe5a0
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-05-08 01:49:44 +0900

    5/8 MVC 템플릿 엔진 공부

--- src/main/java/springboot/springbootbasic/controller/helloController.java
+++ src/main/java/springboot/springbootbasic/controller/helloController.java
@@ -3,6 +3,7 @@ package springboot.springbootbasic.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class helloController {
@@ -10,6 +11,13 @@ public class helloController {
     @GetMapping("hello") // "hello" 라는 url연결 경로
     public String hello(Model model) {
         model.addAttribute("data","hello!!"); // 이름:data ,값:hello!!
-        return "hello"; // return의 "hello" 의미는 hello.html 파일로 연결된다는 의미!
+        return "hello"; // return의 "hello" 의미는 hello.html 파일로 연결된다ㅔ 의미!
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) { // localhost:8080/hello-mvc?name=spring!(name = 파라미터 넘겨줌)
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
+
