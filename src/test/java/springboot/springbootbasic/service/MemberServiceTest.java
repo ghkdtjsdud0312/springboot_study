ed821497f6b0c3b250dccbd87bc42e81a1e3c040
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-05-14 23:39:47 +0900

    5/14 MemberServiceTest 파일 만들기

---
+++ src/test/java/springboot/springbootbasic/service/MemberServiceTest.java
@@ -0,0 +1,21 @@
+package springboot.springbootbasic.service;
+
+import org.junit.jupiter.api.Test;
+
+class MemberServiceTest {
+
+    @Test
+    void join() {
+
+    }
+
+    @Test
+    void findMembers() {
+
+    }
+
+    @Test
+    void findOne() {
+
+    }
+}
