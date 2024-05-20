a3b031e22e8bcd9f7b4d021411df9f6905411821
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-05-20 20:59:52 +0900

    5/20 MemberController GetMapping

--- src/main/java/springboot/springbootbasic/controller/MemberController.java
+++ src/main/java/springboot/springbootbasic/controller/MemberController.java
@@ -2,6 +2,7 @@ package springboot.springbootbasic.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import springboot.springbootbasic.service.MemberService;
 
 // spring이 뜰 때 Controller가 객체를 생성해서 스프링이 가지고 있음
@@ -40,4 +41,8 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
 }
