c2d3d3826930c30009b42d973ad88ac36bb8c6fe
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-05-23 23:49:09 +0900

    5/23 insert 하는 것 설명달기

--- src/main/java/springboot/springbootbasic/repository/JdbcMemberRepository.java
+++ src/main/java/springboot/springbootbasic/repository/JdbcMemberRepository.java
@@ -15,20 +15,22 @@ public class JdbcMemberRepository implements MemberRepository{
     public JdbcMemberRepository(DataSource dataSource) {
         this.dataSource = dataSource;
     }
+
+    // 추가 하기
     @Override
     public Member save(Member member) {
         String sql = "insert into member(name) values(?)";
         Connection conn = null;
         PreparedStatement pstmt = null;
-        ResultSet rs = null;
+        ResultSet rs = null; // 결과를 받는 것
         try {
-            conn = getConnection();
-            pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            pstmt.setString(1, member.getName());
-            pstmt.executeUpdate();
-            rs = pstmt.getGeneratedKeys();
-            if (rs.next()) {
-                member.setId(rs.getLong(1));
+            conn = getConnection(); // getConnection()을 해서 커넥션을 가져온다.
+            pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS); // prepareStatement에 sql을 넣는다. RETURN_GENERATED_KEYS은 DB에 인서트하면 id 값 얻는 것에 쓰임
+            pstmt.setString(1, member.getName()); // setString에서 '파라미터 1번' 이라고 하면 쿼리문의 (?)값에 매칭된다. 그리고 member.getName()에 member.name으로 값을 넣는다.
+            pstmt.executeUpdate(); // DB의 실제 쿼리가 날라간다.
+            rs = pstmt.getGeneratedKeys(); // pstmt.getGeneratedKeys()를 꺼낼 수 있는데 옵션이 Statement.RETURN_GENERATED_KEYS랑 매칭되서 할 수 있다. 그리고 번호에 맞게 반환해준다.
+            if (rs.next()) { // 값이 있으면 꺼낸다.
+                member.setId(rs.getLong(1)); // getLong에서 값을 꺼내고 setId 해주면 된다.
             } else {
                 throw new SQLException("id 조회 실패");
             }
@@ -39,6 +41,8 @@ public class JdbcMemberRepository implements MemberRepository{
             close(conn, pstmt, rs);
         }
     }
+
+    // 조회 하기
     @Override
     public Optional<Member> findById(Long id) {
         String sql = "select * from member where id = ?";
