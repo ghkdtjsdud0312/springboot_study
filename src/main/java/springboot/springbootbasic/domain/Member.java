ef1162ac0542c17abd96ead804135a5a3785afae
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-05-27 21:48:00 +0900

    5/27 JpaMemberRepository 추가

--- src/main/java/springboot/springbootbasic/domain/Member.java
+++ src/main/java/springboot/springbootbasic/domain/Member.java
@@ -1,9 +1,6 @@
 package springboot.springbootbasic.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Member {
@@ -12,6 +9,8 @@ public class Member {
     // 오라클 db는 시퀀스 쓰기도 하고 테이블에서 채번할 수도 있고 pk를 직접 넣어 줄 수도 있지만 db가 알아서 생성해주는 것이 아이덴티티라고 한다.
     @GeneratedValue(strategy = GenerationType.IDENTITY) // db가 자동으로 생성되게 해주는 코드
     private Long id; // 시스템이 저장하는 id
+
+    @Column(name = "username") // db에 있는 컬럼명이 username이라 이게 맵핑이 되는 것이다.
     private String name; // 사용자 이름
 
     // getter & setter 만들기
