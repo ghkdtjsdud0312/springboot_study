528a31a965beb3209201d4f0d00feabeeb7ab395
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-05-28 23:56:29 +0900

    5/28 내용 수정 및 추가

--- src/main/java/springboot/springbootbasic/SpringConfig.java
+++ src/main/java/springboot/springbootbasic/SpringConfig.java
@@ -32,10 +32,10 @@ public class SpringConfig {
         this.memberRepository = memberRepository;
     }
 
-//    @Bean // 수동으로 스프링 빈에 등록하겠다는 의미
-//    public MemberService memberService() {
-//        return new MemberService(memberRepository()); // 멤버 서비스가 스프링빈에 등록이 된다. -> 생성자에 멤버 리포지토리를 연결 시켜 줘야 헤서 memberRepository() 넣어줘야 한다.
-//    }
+    @Bean // 수동으로 스프링 빈에 등록하겠다는 의미
+    public MemberService memberService() {
+        return new MemberService(memberRepository); // 멤버 서비스가 스프링빈에 등록이 된다. -> 생성자에 멤버 리포지토리를 연결 시켜 줘야 헤서 memberRepository() 넣어줘야 한다.
+    }
 
     // 멤버 서비스랑 멤버 리포지토리를 둘 다 스프링빈에 등록하고 등록되어 있는 멤버 리포지토리를 멤버 서비스에 넣어준다.
 //    @Bean
